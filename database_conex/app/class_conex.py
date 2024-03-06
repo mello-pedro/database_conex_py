f17e9894b34fd725d54d16695f65bcc1dc8f7c18
Pedro Pereira <pedro.pereira5894@gmail.com>
2024-03-06 20:52:11 +0000

    versao inicial pronta

---
+++ database_conex/app/class_conex.py
@@ -0,0 +1,56 @@
+import psycopg2
+
+class Conex_database:
+    def __init__(self, host: str, port: str, database: str, user: str, password: str):
+        self.host = host
+        self.port = port
+        self.database = database
+        self.user = user
+        self.password = password
+        self.conn = None
+        self.cur = None
+
+    def connexion(self):
+        try:
+            print('conectando ao banco de dados...')
+            self.conn = psycopg2.connect(
+                host = self.host,
+                port =self.port,
+                database = self.database,
+                user = self.user,
+                password = self.password
+            )
+            print('conexão bem sucedida!')
+        except psycopg2.Error as e:
+            print("Erro ao conectar ao banco de dados: ", e)
+
+    def cursor_conex(self, query: str):
+        try:
+            self.cur = self.conn.cursor()
+            self.cur.execute(query)
+            results = self.cur.fetchall()
+            return results
+        except psycopg2.Error as e:
+            print("Erro ao executar a query: ", e)
+        finally:
+            self.close_cur()
+
+    def save_dataframe(self, results, data_path: str):
+        try:
+            with open(data_path, "w", newline="") as f:
+                # Criar o csv
+                writer = csv.writer(f)
+                writer.writerow([col[0] for col in self.cur.description])
+                writer.writerows(results)
+                print(f'dados exportados com sucesso para {data_path}')
+        except IOError as e:
+            print('A exportação falhou!', e)
+
+    def close_cur(self):
+        if self.cur:
+            self.cur.close()
+
+    def close_conn(self):
+        if self.conn:
+            self.conn.close()
+            print('Conexão fechada com sucesso!')
---
+++ database_conex/app/main.py
@@ -0,0 +1,12 @@
+from class_conex import Conex_database
+
+## Conectar ao database usando a Class Conex_database (colocar senha no lugar do xxxxxxxxx)
+conectar = Conex_database('localhost', '5432', 'northwind', 'pedro', 'xxxxxxx')
+conectar.connexion()
+
+## Fazer a query e guardar os resultados em uma variavel result
+result = conectar.cursor_conex('SELECT * FROM orders')
+
+## Salvar o dataframe na minha área de trabalho
+conectar.save_dataframe(result, '/Users/pedroh.mello/Desktop/northwind_test.csv')
+conectar.close_conn()
